3f3fa82000597073f55de2a3a65b85953f9b4c56
Oliver Beckstein <orbeckst@gmail.com>
2023-06-07 16:38:58 -0700

    updated spectrum

--- Module_3/spectrum.py
+++ Module_3/spectrum.py
@@ -18,23 +18,24 @@ colors = [("red", (625, 750)),
 
 if __name__ == "__main__":
 
-    # compute energies corresponding toe lower and upper wavelengths
+    # compute energies corresponding to lower and upper wavelengths
     # 1. convert input wavelength nm to m
     # 2. convert output energy from J to eV
-    energies = [(J2eV(photonenergycalc.photon_energy(lambdas[0] * 1e-9)),
-                 J2eV(photonenergycalc.photon_energy(lambdas[1] * 1e-9)))
-                for name, lambdas in colors]
+    photonenergies = [(J2eV(photonenergycalc.photon_energy(lambdas[0] * 1e-9)),
+                       J2eV(photonenergycalc.photon_energy(lambdas[1] * 1e-9)))
+                      for name, lambdas in colors]
 
-    # iterate through colors and energies together (in reverse order
-    # so that violet is at top) to pull out the relevant data and
-    # pretty print (note that lower energy belongs to higher wavelength!)
+    # iterate through colors and photonenergies together (in reverse
+    # order so that violet is at top) to pull out the relevant data
+    # and pretty-print (note that lower energy belongs to higher
+    # wavelength!)
 
     header = "{name:10s} {wavelength:12s} {energy:9s}".format(
         name="color", wavelength="wavelength (nm)", energy="energy (eV)")
     print(header)
     print("-" * len(header))
     for (name, lambdas), energies in zip(reversed(colors),
-                                       reversed(energies)):
+                                         reversed(photonenergies)):
         print(f"{name:10s}     "
               f"{lambdas[0]:3g}-{lambdas[1]:3g}      "
               f"{energies[1]:4.2f}-{energies[0]:4.2f}")
